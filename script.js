e8a63aa241502b048d3debbf8d49f4cfcd9c16cc
RadNaw1986 <rnawrot704@gmail.com>
2024-06-26 14:28:09 +0200

    Finalizacja wysyłki formularza

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
 // Skrypt do obsługi formularza zgłoszeniowego
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('#registration-form');
+const form = document.querySelector('#registration-form');
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        
-        // Pobierz wartości z pól formularza
-        const formData = new FormData(form);
-
-        // Wysłanie danych za pomocą Fetch API
-        fetch(form.action, {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Wystąpił problem podczas wysyłania zgłoszenia.');
-            }
-            return response.text(); // Zmieniono na `response.text()` dla obsługi zwykłych odpowiedzi
-        })
-        .then(data => {
-            console.log('Odpowiedź serwera:', data);
-            alert('Zgłoszenie zostało pomyślnie wysłane!');
-            form.reset(); // Wyczyść formularz po wysłaniu
-        })
-        .catch(error => {
-            console.error('Błąd:', error);
-            alert('Wystąpił problem podczas wysyłania zgłoszenia. Spróbuj ponownie później.');
-        });
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Pobierz wartości z pól formularza
+    const name = document.querySelector('#name').value;
+    const affiliation = document.querySelector('#affiliation').value;
+    const academicTitle = document.querySelector('#academic-title').value;
+    const abstract = document.querySelector('#abstract').value;
+    const email = document.querySelector('#email').value;
+    const phone = document.querySelector('#phone').value;
+    
+    // Obiekt z danymi do wysłania
+    const formData = {
+        name: name,
+        affiliation: affiliation,
+        academicTitle: academicTitle,
+        abstract: abstract,
+        email: email,
+        phone: phone
+    };
+    
+    // Wysłanie danych za pomocą Fetch API
+    fetch('https://radnaw.pl/zapisz_zgloszenie.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Wystąpił problem podczas wysyłania zgłoszenia.');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Odpowiedź serwera:', data);
+        alert('Zgłoszenie zostało pomyślnie wysłane!');
+        form.reset(); // Opcjonalnie: Wyczyść formularz po wysłaniu
+    })
+    .catch(error => {
+        console.error('Błąd:', error);
+        alert('Wystąpił problem podczas wysyłania zgłoszenia. Spróbuj ponownie później.');
     });
 });
