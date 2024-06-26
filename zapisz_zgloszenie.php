4423d1a2ec767a52b7b7a1f6a8b354f7fa639604
RadNaw1986 <rnawrot704@gmail.com>
2024-06-26 15:00:33 +0200

    podmiana strony z podziekowaniem

--- zapisz_zgloszenie.php
+++ zapisz_zgloszenie.php
@@ -1,4 +1,7 @@
 <?php
+
+//to jest plik formularza zgłoszeniowego, który znajduje sie na moim serwerze - załaczony jedynie poglądowo do projektu
+
 // Sprawdzenie czy dane zostały wysłane metodą POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
@@ -13,10 +16,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Adres email, na który będą wysyłane zgłoszenia
     $to = 'rnawrot704@gmail.com';
 
-    // Temat wiadomości email
+    // Temat wiadomości email dla zgłoszenia
     $subject = 'Nowe zgłoszenie na konferencję';
 
-    // Treść wiadomości email
+    // Treść wiadomości email dla zgłoszenia
     $message = "Nowe zgłoszenie na konferencję:\n\n";
     $message .= "Imię i nazwisko: $name\n";
     $message .= "Afiliacja: $affiliation\n";
@@ -25,18 +28,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message .= "Email: $email\n";
     $message .= "Nr telefonu: $phone\n";
 
-    // Nagłówki wiadomości email
+    // Nagłówki wiadomości email dla zgłoszenia
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    // Wysłanie wiadomości email
+    // Wysłanie wiadomości email dla zgłoszenia
     if (mail($to, $subject, $message, $headers)) {
-        // Przekierowanie użytkownika po udanym wysłaniu formularza
-        header('Location: https://radnaw.pl/podziekowanie.html');
-        exit;
+        // Przygotowanie wiadomości potwierdzającej dla zgłaszającego
+        $confirmSubject = 'Potwierdzenie zgłoszenia na konferencję';
+        $confirmMessage = "Dziękujemy za zgłoszenie na konferencję.\n\n";
+        $confirmMessage .= "Otrzymaliśmy następujące dane:\n\n";
+        $confirmMessage .= "Imię i nazwisko: $name\n";
+        $confirmMessage .= "Afiliacja: $affiliation\n";
+        $confirmMessage .= "Tytuł naukowy: $academicTitle\n";
+        $confirmMessage .= "Abstrakt: $abstract\n";
+        $confirmMessage .= "Email: $email\n";
+        $confirmMessage .= "Nr telefonu: $phone\n";
+
+        // Wysłanie wiadomości potwierdzającej do zgłaszającego
+        if (mail($email, $confirmSubject, $confirmMessage, $headers)) {
+            // Przekierowanie użytkownika po udanym wysłaniu formularza
+            header('Location: https://radnaw.pl/podziekowanie');
+            exit;
+        } else {
+            // Obsługa błędu, jeśli nie udało się wysłać wiadomości potwierdzającej
+            echo 'Wystąpił problem podczas przetwarzania zgłoszenia. Spróbuj ponownie później.';
+        }
     } else {
-        // Obsługa błędu, jeśli nie udało się wysłać wiadomości
+        // Obsługa błędu, jeśli nie udało się wysłać wiadomości zgłoszenia
         echo 'Wystąpił problem podczas przetwarzania zgłoszenia. Spróbuj ponownie później.';
     }
 } else {
